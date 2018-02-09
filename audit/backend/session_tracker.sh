d4b0d97759e479edb9f8c034592f9061fc9ebf66
skeegv <skeegv@gmail.com>
2018-02-09 11:33:19 +0800

    新增循环监听会话 sh 脚本.

---
+++ audit/backend/session_tracker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# for loop 30.get process id by  random tag,
+# if got the process id , start strace command!
+
+for i in $(seq 1 30);do
+    process_id=`ps -ef|grep $1|egrep -v "sshpass|grep|session_tracker.sh"|awk '{print $2}'`
+    echo "process:$process_id"
+    if [ ! -z "$process_id" ];then
+         echo 'start run strace...'
+         strace -ftp $process_id -o ssh_audit.log;
+         break;
+    fi
+    sleep 5
+done;
+
