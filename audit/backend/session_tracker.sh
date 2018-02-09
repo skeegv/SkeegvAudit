6da572a486ff904ba2710b9f647f2e1f788e4ba7
skeegv <skeegv@gmail.com>
2018-02-09 21:15:28 +0800

    完善 session_tracker logs 日志

--- audit/backend/session_tracker.sh
+++ audit/backend/session_tracker.sh
@@ -8,9 +8,10 @@ for i in $(seq 1 30);do
     echo "process:$process_id"
     if [ ! -z "$process_id" ];then
          echo 'start run strace...'
-         sudo strace -ftp $process_id -o ../../logs/ssh_audit_$2.log;
+         #  logs/ 目录需要我们在 home/audit/下创建
+         sudo strace -ftp $process_id -o logs/ssh_audit_$2.log;
          break;
     fi
-    sleep 5
+    sleep 1
 done;
 
